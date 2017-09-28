60c75a7930d3e7d0778f5a9f3cc5cbdae2b2f34e
Tenzing Sherpa <ttsherpa@umail.ucsb.edu>
2017-09-28 14:25:37 -0700

    updated db

--- config/database.js
+++ config/database.js
@@ -2,8 +2,9 @@ const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides c
 
 // Export config object
 module.exports = {
-  uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
-  // uri: 'mongodb://ttsherpa:Sbtibetan1996@ds153494.mlab.com:53494/blogger-world',//production
+  // uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
+  uri: 'mongodb://ttsherpa:Sbtibetan1996@ds153494.mlab.com:53494/blogger-world',//production
   secret: crypto, // Cryto-created secret
-  db: 'mean-angular-2' // Database name
+  // db: 'mean-angular-2' // Database name
+  db: 'blogger-world' // Database name
 }
